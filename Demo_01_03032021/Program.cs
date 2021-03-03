7183dc5d25a384280041428e8518f47019d9764a
Christian <schenkenberger.ch@gmail.com>
2021-03-03 08:40:39 +0100

    WIP
    123

---
+++ Demo_01_03032021/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_01_03032021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 0;
+        }
+    }
+}
