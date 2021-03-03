9edeef42ba628db44e3b64153de3fec6b0eec278
Christian <schenkenberger.ch@gmail.com>
2021-03-03 13:12:34 +0100

    WIP

--- Demo_03_03032021/Program.cs
+++ Demo_03_03032021/Program.cs
@@ -10,6 +10,47 @@ namespace Demo_03_03032021
     {
         static void Main(string[] args)
         {
+            Screen screen = new Screen();
+            SpieleKonsole spieleKonsole = new SpieleKonsole();
+            Receiver receiver = new Receiver();
+            spieleKonsole.istAn = true;
+            receiver.istAn = false;
+            Console.WriteLine(screen.Channel(receiver, spieleKonsole));
+            Console.ReadLine();
+
         }
+    }
+    class Screen
+    {
+        public string kanal;
+        public bool istAn;
+
+        public string Channel(Receiver r, SpieleKonsole s)
+        {
+            if (r.istAn == true)
+            {
+                kanal = "Reviever läuft läuft. Setze Kanal auf 1.";
+                istAn = true;
+            }
+            if (s.istAn == true)
+            {
+                kanal = "Spielekonsole läuft. Setze Kanal auf 2.";
+            }
+            else
+            {
+                kanal = "Kein Signal";
+            }
+            return kanal;
+        }
+    }
+    public class Receiver
+    {
+        public bool istAn;
+
+    }
+    class SpieleKonsole
+    {
+        public bool istAn;
+
     }
 }
