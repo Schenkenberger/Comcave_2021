62007f746744d19b19397740ab5a836b529a4499
Christian <schenkenberger.ch@gmail.com>
2021-03-03 09:13:45 +0100

    WIP

--- Demo_01_03032021/Program.cs
+++ Demo_01_03032021/Program.cs
@@ -11,36 +11,39 @@ namespace Demo_01_03032021
         static void Main(string[] args)
         {
             Kind test = new Kind();
-            test.setName("Bla");
+            test.name = "Herbert";
+            test.alter = 22;
             test.nachname = "Test";
-            Console.WriteLine(test.getName());
+            Console.WriteLine(test.name);
+            Console.WriteLine(test.alter);
             Console.WriteLine(test.nachname);
             Console.ReadLine();
         }
     }
     class Mutter
     {
-        private string name;
-        private int alter;
+        //private string name;
+        //private int alter;
 
         public string nachname { get; set; }
-
-        public string getName()
-        {
-            return name;
-        }
-        public int getAlter()
-        {
-            return alter;
-        }
-        public void setAlter(int alter)
-        {
-            this.alter = alter;
-        }
-        public void setName(string name)
-        {
-            this.name = name;
-        }
+        public int alter { get; set; }
+        public string name { get; set; }
+        //public string getName()
+        //{
+        //    return name;
+        //}
+        //public int getAlter()
+        //{
+        //    return alter;
+        //}
+        //public void setAlter(int alter)
+        //{
+        //    this.alter = alter;
+        //}
+        //public void setName(string name)
+        //{
+        //    this.name = name;
+        //}
     }
     class Kind : Mutter
     {
